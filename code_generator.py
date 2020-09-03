48eebaef3ce505ff29ed104916bf1b7b70a7f4cc
Alex Pirciu <alex.pirciu@zohomail.eu>
2020-09-03 21:29:51 +0300

    Read the files in the provided path and select the metadata file from the rest of the files

--- code_generator.py
+++ code_generator.py
@@ -1,31 +1,41 @@
 import importlib
 import sys
 import os
+import json
 
 
 def main(argv):
     if(not argv):
-        readme = open("README.md")
-        readme_content = readme.read()
-        readme.close()
+        with open("README.md") as readme:
+            readme_content = readme.read()
         # TODO: if there are no arguments provided,
         # take the #usage section of the README file and print it
         print(readme_content)
     else:
-        print(argv[0])
         if not os.path.isdir(argv[0]):
             print("Please provide a valid path")
             return
 
+        files = []
+        for f in os.listdir(argv[0]):
+            fileToAdd = os.path.join(argv[0], f)
+            if(f == "metadata.json"):
+                global metadata
+                metadata = fileToAdd
+            else:
+                files.append(fileToAdd)
+
+        print(metadata)
+        for f in files:
+            print(f)
         # TODO: read metadata.json for information about the languages to
         # execute the script for and the paths to write the generated files to
 
         # langs = input("provide the languages\n")
         # langs = langs.split(",")
 
-        # TODO: for each txt file containing the metacode,
-        # generate the specific class for each language from the metadata
-        # file
+        # TODO: for each file containing the metacode, generate the specific
+        # class for each language from the metadata file
 
         # modules = []
         # for lang in langs:
